7d4803f0b1f3951104b305163f67107b221f4b17
dimas <dprcontractor@gmail.com>
2022-04-04 20:04:02 -0400

    set up connection with dotenv

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2')
+
+require('dotenv').config();
+
+const db = mysql.createConnection(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    
+    }
+)
+
+module.exports = db;
